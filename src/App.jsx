e058ab9c048b19759497f4fb763fa63afed222c4
actionxbar-14 <ankitpaths144@gmail.com>
2025-04-22 12:21:03 +0530

    style added commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ const App = () => {
         pauseOnHover={false}
       />
 
-      <h1 className="bg-[#1c1c1c] p-4 text-white text-center text-3xl">
+      <h1 className="bg-[#1c1c1c] p-4 font-extrabold text-white text-center text-4xl">
         Task Manager
       </h1>
-      <h1 className="text-2xl font-extrabold text-white text-center mb-6 bg-white/10 backdrop-blur-md py-3 px-6 rounded-md border border-white/30 shadow-md">
+      <h1 className="text-xl  text-white text-center mb-6 bg-white/10 backdrop-blur-md py-3 px-6 rounded-md border border-white/30 shadow-md">
          Add Your Task and delete Easily
       </h1>
       <InputForm />
